27a157b5e87d4e2d6178e20a14d8d77a44d680fd
zvikinoza <zvikinoza@gmail.com>
2022-04-06 01:38:50 +0400

    fix inf iterator

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,8 @@ pub struct Flatten<O>
         O::Item: IntoIterator,
 {
     outer: O,
-    inner: Option<<O::Item as IntoIterator>::IntoIter>,
+    front_inner: Option<<O::Item as IntoIterator>::IntoIter>,
+    back_inner: Option<<O::Item as IntoIterator>::IntoIter>,
 }
 
 impl<O> Flatten<O>
@@ -22,7 +23,11 @@ impl<O> Flatten<O>
         O::Item: IntoIterator,
 {
     fn new(iter: O) -> Self {
-        Flatten { outer: iter, inner: None}
+        Flatten {
+            outer: iter,
+            front_inner: None,
+            back_inner: None,
+        }
     }
 }
 
@@ -34,14 +39,17 @@ impl <O> Iterator for Flatten<O>
     type Item = <O::Item as IntoIterator>::Item;
     fn next(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(ref mut inner) = self.inner {
-                if let Some(i) = inner.next() {
+            if let Some(ref mut front_inner) = self.front_inner {
+                if let Some(i) = front_inner.next() {
                     return Some(i);
                 }
-                self.inner = None;
+                self.front_inner = None;
             }
-            let next_inner_it = self.outer.next()?.into_iter();
-            self.inner = Some(next_inner_it);
+            if let Some(out) = self.outer.next() {
+                self.front_inner = Some(out.into_iter());
+            } else {
+                return self.back_inner.as_mut()?.next();
+            }
         }
     }
 }
@@ -54,14 +62,17 @@ impl<O> DoubleEndedIterator for Flatten<O>
 {
     fn next_back(&mut self) -> Option<Self::Item> {
         loop {
-            if let Some(ref mut inner) = self.inner {
-                if let Some(i) = inner.next_back() {
+            if let Some(ref mut back_inner) = self.back_inner {
+                if let Some(i) = back_inner.next_back() {
                     return Some(i);
                 }
-                self.inner = None;
+                self.back_inner = None;
             }
-            let next_inner_it = self.outer.next_back()?.into_iter();
-            self.inner = Some(next_inner_it);
+            if let Some(out) = self.outer.next_back() {
+                self.back_inner = Some(out.into_iter());
+            } else {
+                return self.front_inner.as_mut()?.next_back();
+            }
         }
     }
 }
@@ -69,10 +80,12 @@ impl<O> DoubleEndedIterator for Flatten<O>
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn empty() {
         assert_eq!(flatten(std::iter::empty::<Vec<()>>()).count(), 0);
     }
+
     #[test]
     fn one() {
         assert_eq!(flatten(std::iter::once(vec!["a"])).count(), 1);
@@ -102,6 +115,7 @@ mod tests {
             vec!["b", "a"]
         );
     }
+
     #[test]
     fn reverse_deep() {
         assert_eq!(
@@ -111,4 +125,31 @@ mod tests {
             vec!["b", "a"]);
     }
 
+    #[test]
+    fn next_and_next_back() {
+        let mut iter = flatten(vec![vec!["a", "b"], vec!["c", "d"]]);
+        assert_eq!(iter.next(), Some("a"));
+        assert_eq!(iter.next_back(), Some("d"));
+        assert_eq!(iter.next(), Some("b"));
+        assert_eq!(iter.next_back(), Some("c"));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next_back(), None);
+    }
+
+    #[test]
+    fn infinite_iter() {
+        let mut iter = flatten((0..).map(|i| { 0..i }));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+    }
+
+    #[test]
+    fn we_need_to_go_deeper() {
+        let deep = vec![vec![vec![vec![vec![1, 2]], vec![vec![3, 4]]]]];
+        assert_eq!(flatten(flatten(flatten(flatten(deep)))).count(), 4);
+    }
 }
